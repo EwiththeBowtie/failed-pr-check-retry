d3c9cfb97bf2af368ff38473409c78c1f296ca2c
E Einowski <eeinowski@cloudbolt.io>
2023-04-02 20:25:41 -0700

    🚀 feat(index.js): add functionality to rerun failed workflows This commit adds functionality to rerun failed workflows. The script retrieves failed checks for open pull requests, extracts the failed checks, and then uses the Octokit API to get run details from the details_url. It then calls the rerun_url to rerun the failed jobs. The script takes in parameters such as owner, repo, token, and maxRetries.

--- index.js
+++ index.js
@@ -1,21 +1,125 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { Octokit } = require("@octokit/rest");
+const Immutable = require("immutable");
+const moment = require("moment");
 
 // most @actions toolkit packages have async methods
-async function run() {
+async function run({ owner, repo, token, maxRetries, retryInterval }) {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    // Set the time range to retrieve failed checks for (last 24 hours)
+    const now = new Date();
+    const yesterday = new Date(now.getTime() - 24 * 60 * 60 * 1000 * 30);
+    const since = yesterday.toISOString();
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    // Create a new Octokit instance with an authentication token (if needed)
+    const octokit = new Octokit({
+      auth: token,
+    });
 
-    core.setOutput('time', new Date().toTimeString());
+    // get open check status for open prs
+    octokit.pulls
+      .list({
+        owner,
+        repo,
+        state: "open",
+      })
+      .then(({ data }) => {
+        // Extract the failed checks from the API response
+        data.forEach((pr) => {
+          octokit.checks
+            .listForRef({
+              owner,
+              repo,
+              ref: pr.head.sha,
+              status: "completed",
+              filter: "latest",
+            })
+            .then((data) => {
+              // Extract the failed checks from the API response
+              const checks = Immutable.fromJS(data);
+              const failedChecks = checks
+                .getIn(["data", "check_runs"])
+                .filter((check) => check.getIn(["conclusion"]) === "failure")
+                .filter((check) => check.getIn(["completed_at"]) > since)
+                .map((check) =>
+                  Immutable.Map({
+                    pr: check.getIn(["pull_requests", 0, "number"]),
+                    name: check.getIn(["name"]),
+                    details_url: check.getIn(["details_url"]),
+                    completed_at: check.getIn(["completed_at"]),
+                    time_since: moment(check.getIn(["completed_at"])).fromNow(),
+                    conclusion: check.getIn(["conclusion"]),
+                    run_id: check.getIn(["details_url"]).split("/")[7],
+                    job_id: check.getIn(["details_url"]).split("/")[9],
+                  })
+                );
+
+              //pretty print failed checks
+              // console.log(failedChecks.toJS());
+              return failedChecks;
+            })
+            .then((failedChecks) => {
+              // use octokit to get run details from the details_url
+              const runDetails = failedChecks.map((check) => {
+                return octokit.actions
+                  .getWorkflowRun({
+                    owner,
+                    repo,
+                    run_id: check.get("run_id"),
+                  })
+                  .then((data) => {
+                    const run = Immutable.fromJS(data);
+                    const candidates = Immutable.Map({
+                      run_attempt: run.getIn(["data", "run_attempt"]),
+                      name: run.getIn(["data", "name"]),
+                      workflow_file: run.getIn(["data", "path"]),
+                      run_number: run.getIn(["data", "run_number"]),
+                      status: run.getIn(["data", "status"]),
+                      conclusion: run.getIn(["data", "conclusion"]),
+                      workflow_id: run.getIn(["data", "workflow_id"]),
+                      rerun_url: run.getIn(["data", "rerun_url"]),
+                      run_id: run.getIn(["data", "id"]),
+                    });
+                    // console.log(candidates.toJS());
+                    return candidates;
+                  });
+              });
+              return Promise.all(runDetails);
+            })
+            .then((runDetails) => [].concat(...runDetails))
+            .then((runDetails) => {
+              return Promise.all(
+                runDetails.map((run) => {
+                  // use octokit to call rerun_url
+                  console.log("run", run.toJS());
+                  return octokit.actions.reRunWorkflowFailedJobs({
+                    owner,
+                    repo,
+                    run_id: run.get("run_id"),
+                  });
+                })
+              );
+            })
+            .then((data) => {
+              data.map((run) => {
+                console.log("rerun", run.toJS());
+              });
+            })
+            .catch((error) => {
+              // handle error
+              console.log(error);
+            });
+        });
+      });
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run();
+run({
+  owner: core.getInput("owner"),
+  repo: core.getInput("repo"),
+  token: core.getInput("token"),
+  maxRetries: core.getInput("maxRetries"),
+});
